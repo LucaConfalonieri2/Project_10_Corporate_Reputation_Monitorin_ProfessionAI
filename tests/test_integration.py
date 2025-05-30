722094c8295d8958e3b3b0f776de5f08d972ba8a
LucaConfalonieri <luca2.confalonieri@mail.polimi.it>
2025-05-30 12:12:54 +0200

    update TI

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,25 +1,24 @@
 import os
 import subprocess
-import src.utils as utils
 
 def test_batch_manager_crea_csv():
-    assert os.path.exists(utils.TRAIN_DATASET_FILE), f"{utils.TRAIN_DATASET_FILE} not exits"
-    assert os.path.exists(utils.TEST_DATASET_FILE), f"{utils.TEST_DATASET_FILE} not exits"
+    assert os.path.exists("data/processed/train.csv"), f"data/processed/train.csv not exits"
+    assert os.path.exists("data/processed/train.csv"), f"data/processed/train.csv not exits"
 
-    with open(utils.TRAIN_DATASET_TEMP) as f:
+    with open("data/new/new_train_data.csv") as f:
         old_content_train = f.read()
-    with open(utils.TEST_DATASET_TEMP) as f:
+    with open("data/new/new_test_data.csv") as f:
         old_content_test = f.read()
 
     subprocess.run(["python", "src/batch_manager.py"], check=True)
     
-    with open(utils.TRAIN_DATASET_TEMP) as f:
+    with open("data/new/new_train_data.csv") as f:
         new_content_train = f.read()
-    with open(utils.TEST_DATASET_TEMP) as f:
+    with open("data/new/new_test_data.csv") as f:
         new_content_test = f.read()
 
-    assert old_content_train != new_content_train, f"{utils.TRAIN_DATASET_TEMP} not change"
-    assert old_content_test != new_content_test, f"{utils.TEST_DATASET_TEMP} not change"
+    assert old_content_train != new_content_train, f"data/new/new_train_data.csv not change"
+    assert old_content_test != new_content_test, f"data/new/new_test_data.csv not change"
 
 def test_train_and_test_model():
     subprocess.run(["python", "src/train.py"], check=True)
@@ -28,12 +27,12 @@ def test_train_and_test_model():
                       "tokenizer_config.json", "vocab.json"
                       ]
     for f in expected_files:
-        full_path = os.path.join(utils.MODEL_PATH, f)
+        full_path = os.path.join("models/sentiment_model", f)
         assert os.path.isfile(full_path), f"File mancante nel modello: {f}"
 
-    assert os.path.exists(utils.LOG_FILE)
+    assert os.path.exists("logs/eval_log.csv")
 
-    with open(utils.LOG_FILE, 'r') as f:
+    with open("logs/eval_log.csv", 'r') as f:
         old_content = f.read()
 
     subprocess.run(
@@ -41,7 +40,7 @@ def test_train_and_test_model():
         env={**os.environ, "DISABLE_UPLOAD": "1"}
     )
     
-    with open(utils.LOG_FILE, 'r') as f:
+    with open("logs/eval_log.csv", 'r') as f:
         new_content = f.read()
 
-    assert old_content != new_content, "Unsaved metrics"
+    assert old_content != new_content, "Unsaved metrics in logs/eval_log.csv"
